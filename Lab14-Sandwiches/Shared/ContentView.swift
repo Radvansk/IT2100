a8378719abdfe3b34da3e7df2615736394c6a964
John Radvansky <radvansky.john@gmail.com>
2021-04-25 21:37:15 -0400

    Lab 14 sandwiches complete

---
+++ Lab14-Sandwiches/Shared/ContentView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct ContentView: View {
+    var sandwiches: [Sandwich] = []
+    
+    var body: some View {
+        NavigationView {
+            List(sandwiches) { sandwich in
+                NavigationLink(destination: Text(sandwich.name)) {
+                    Image(sandwich.thumbnailName)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading) {
+                        Text(sandwich.name)
+                        Text("\(sandwich.ingredientCount) ingredients")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Sandwiches")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+       ContentView(sandwiches: testData)
+    }
+}
+
